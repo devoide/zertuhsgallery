9a95712ca181c807228dfaa053a045d63d21d879
devoide <chefmocci@gmail.com>
2025-10-01 13:44:44 +0200

    add vite to gitignoer

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import { Text } from "@chakra-ui/react";
 
 export function NotFound() {
-  return <Text>This place was still not discovered by zertuh.</Text>;
+  return <Text>ts place was still not discovered by zertuh.</Text>;
 }
 
 export default NotFound;
